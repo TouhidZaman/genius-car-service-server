01e9592811ea265593084cae137c3a4815cfd272
Muhammad Touhiduzzaman <touhid4bd@gmail.com>
2022-04-24 15:39:14 +0600

    insert single/multiple services, update service and delete service added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-//middleware
+//Middleware
 app.use(cors());
 app.use(express.json());
 
@@ -23,6 +23,10 @@ async function run() {
         await client.connect();
         const servicesCollection = client.db("geniusCarDB").collection("services");
 
+        //////////////////////////////////
+        // Find Operations
+        //////////////////////////////////
+
         //Getting services using search query
         app.get("/services", async (req, res) => {
             const query = {}; //Optimization needed
@@ -30,23 +34,76 @@ async function run() {
             const services = await cursor.toArray();
             if (req.query.name) {
                 const searchText = req.query.name.toLowerCase();
-                const filteredServices = services.filter(service => service.name.toLowerCase().includes(searchText));
+                const filteredServices = services.filter((service) =>
+                    service.name.toLowerCase().includes(searchText)
+                );
                 res.send(filteredServices);
-            }
-            else {
+            } else {
                 res.send(services);
             }
-
         });
 
         //Getting a single service using id parameter
         app.get("/service/:id", async (req, res) => {
             const serviceId = req.params.id;
-            console.log(serviceId);
             const query = { _id: ObjectId(serviceId) };
             const service = await servicesCollection.findOne(query);
             res.send(service);
         });
+
+        //////////////////////////////////
+        // Insert Operations
+        //////////////////////////////////
+
+        //Adding a service
+        app.post("/service", async (req, res) => {
+            const newService = req.body;
+            const result = await servicesCollection.insertOne(newService);
+            res.send(result); // will return insertedId
+        });
+
+        //Adding multiple service
+        app.post("/services", async (req, res) => {
+            const newService = req.body;
+            const result = await servicesCollection.insertMany(newService);
+            res.send(result); //will return insertedCount, insertedIds
+        });
+
+        //////////////////////////////////
+        // Update Operations
+        //////////////////////////////////
+
+        //Updating a service
+        app.put("/service/:id", async (req, res) => {
+            const serviceId = req.params.id;
+            const filter = { _id: ObjectId(serviceId) }; //Finding existing service using id
+
+            //formatting updated service
+            const updatedService = req.body;
+            const updateDoc = {
+                $set: {
+                    ...updatedService,
+                },
+            };
+
+            const options = { upsert: true }; //update option (update/insert)
+
+            const result = await servicesCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+        });
+
+        //////////////////////////////////
+        // Delete Operations
+        //////////////////////////////////
+
+        //Deleting a service
+        app.delete("/service/:id", async (req, res) => {
+            const serviceId = req.params.id;
+            const query = { _id: ObjectId(serviceId) };
+            const result = await servicesCollection.deleteOne(query);
+            res.send(result);
+        });
+        //
     } finally {
         // await client.close();
     }
